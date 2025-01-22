86e3190c02603893396dc27c5d858bded559919e
kong <kong504_25@hotmail.co.th>
2025-01-22 10:59:13 +0700

    update: project stauts

---
+++ program/program-api.ts
@@ -0,0 +1,12 @@
+type program_api = {
+    target: string;
+    name: string;
+    year:{
+        startyear: string;
+        endyear: string;
+    },
+    budget: number;
+    own: string;
+    plan: string;
+    status: number;
+}[];
