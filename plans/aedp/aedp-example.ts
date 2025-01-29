1a419a8f8b1085c5cfdea7fe43fc2b8ab60b5bbf
kong <kong504_25@hotmail.co.th>
2025-01-29 15:29:41 +0700

    add: example aedp, eep

---
+++ plans/aedp/aedp-example.ts
@@ -0,0 +1,15 @@
+const aedpexample = {
+    charts: {
+        alt: [...],
+        co2: [...],
+        renewable_energy_ratio: [
+            {
+                year: '2567',
+                target: 20,
+                result: 10,
+            },
+            ...
+        ],
+    }
+}
+
---
+++ plans/eep/eep-example.ts
@@ -0,0 +1,19 @@
+const eepExample = 
+{
+    summary: [...],
+    target1: [...],
+    target2: [...],
+    target3: [...],
+    standard_charts: [...],
+    energyOptimization: [
+        {
+            year: 2567,
+            target: 20,
+            result: 10,
+        },
+        ...
+    ],
+    charts: {...}
+}
+
+
